11817d92ac02996568efcf40728ecec2b7d68967
TopprCodr <72185815+TopprCodr@users.noreply.github.com>
2021-07-02 17:46:03 +0530

    Update ball.js

--- ball.js
+++ ball.js
@@ -7,7 +7,7 @@ constructor(x, y, r) {
     }
     this.body = Bodies.circle(x,y,r,options);
      this.r = r
-    World.add(world, this.body);
+    Composite.add(world, this.body);
   }
   display(){
     var pos =this.body.position;
